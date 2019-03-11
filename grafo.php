847062dd71558562003961ce9c7f11e43d8c10b8
Genarodaniel <37519144+Genarodaniel@users.noreply.github.com>
2019-03-11 19:09:33 -0300

    An update of functions and design
    Update of archives

--- grafo.php
+++ grafo.php
@@ -1,50 +1,103 @@
 <?php
 
 // não pode ter casas que nao existem, tipo  [1][0];
-$grafo = array();
 
-$qtde_v = 3;
-$arestas = 9;
-$max_a = $qtde_v * $arestas;
+$qtde_v = isset($_POST["qtde_v"]) ? $_POST["qtde_v"] : null;
+$qtde_a = isset($_POST["qtde_a"]) ? $_POST["qtde_a"] : null;
+$graph_type = isset($_POST["graph_type"]) ? $_POST["graph_type"] : null;
 
-$grafo[0][1] = 0;
-$grafo[0][2] = 0;
-$grafo[0][3] = 0;
-$grafo[1][1] = 1;
-$grafo[1][2] = 1;
-$grafo[1][3] = 1;
-$grafo[2][1] = 1;
-$grafo[2][3] = 1;
-$grafo[2][2] = 1;
-$grafo[3][1] = 1;
+function geraMatrizGrafo() {
 
-foreach ($grafo as $row) {
-	// entra no vetor grafo trazendo as posições para row
+}
 
-	foreach ($row as $key => $value) {
-//entra na row trazendo as posicoes para key e o valor para value
+function imprime() {
 
-		for ($i = 0; $i <= $qtde_v; $i++) {
-// vai rodar esse vetor até no maximo a qtde de vertices
+}
 
-			if (!isset($grafo[$key][$i])) {
-//verifica se foi setado um valor para todas posicoes possiveis
+function recebe($qtde_v, $qtde_a, $graph_type) {
 
-				$grafo[$key][$i] = 0;
+	$qtde_v = trim($qtde_v);
+	$qtde_a = trim($qtde_a);
+	$graph_type = trim($graph_type);
+
+	if ($graph_type == 2) {
+		$digrafo = array();
+		$max_a = $qtde_v * $qtde_v / 2;
+
+		if ($qtde_a <= $max_a) {
+			for ($cont2 = 0; $cont2 <= $qtde_v; $cont2++) {
+
+				for ($i = 0; $i <= $qtde_v; $i++) {
+					$digrafo[$cont2][$i] = 0;
+				}
+
+			}
+			return $digrafo;
+		} elseif ($qtde_a > $max_a) {
+			return "Quantidade de arestas maior que o permitido para digrafo";
+		} elseif ($qtde_a == 0) {
+			return "Quantidade de arestas não definida";
+		}
+	} elseif ($graph_type = 1) {
+		$grafo = array();
+		if ($qtde_a == 0) {
+			return "Quantidade de arestas não definida";
+		}
+		$max_a = $qtde_v * $qtde_v;
+
+		if ($qtde_a <= $max_a) {
+			for ($cont2 = 0; $cont2 <= $qtde_v; $cont2++) {
+
+				for ($i = 0; $i <= $qtde_v; $i++) {
+					$grafo[$cont2][$i] = 0;
+				}
+
+			}
+			return $grafo;
+
+		} elseif ($qtde_a > $max_a) {
+			return "Quantidade de arestas maior que o permitido para grafo";
+		} 
+
+	}
+
+}
+
+var_dump(recebe($qtde_v, $qtde_a, $graph_type));
+
+function geraMatrizDigrafo() {
+
+	/*foreach ($digrafo as $row) {
+		// entra no vetor digrafo trazendo as posições para row
+
+		foreach ($row as $key => $value) {
+			//entra na row trazendo as posicoes para key e o valor para value
+
+			for ($i = 0; $i <= $qtde_v; $i++) {
+				// vai rodar esse vetor até no maximo a qtde de vertices
+
+				if (!isset($digrafo[$key][$i])) {
+					//verifica se foi setado um valor para todas posicoes possiveis
+					$digrafo[$key][$i] = 0;
+
+				}
 			}
 		}
-	}
+	}*/
 
 }
 
 // variaveis origem e destino devem ser diferentes de zero e menor
 // ou igual $qtde_v
+?>
 
-?><table><tbody><?php
+
+
+	<!-- <table><tbody><?php/*
+
 
 $cont = 0;
-
-foreach ($grafo as $row) {
+foreach ($digrafo as $row) {
 	echo "<th>$cont</th>";
 
 	if ($cont == 0) {
@@ -57,6 +110,7 @@ foreach ($grafo as $row) {
 		foreach ($row as $i => $valor) {
 			if ($i > 0 && $i <= $qtde_v) {
 				echo "<td>" . "$valor" . "</td>";
+
 			}
 		}
 	}
@@ -65,7 +119,9 @@ foreach ($grafo as $row) {
 
 	$cont++;
 }
-
-?>
-
+ */ ?> -->
 </table>
+
+
+
+
--- js/grafo.js
+++ js/grafo.js
@@ -1,3 +1,5 @@
+
+
 function valida_tipo(){
 
 		var qtde_v_field =document.getElementById('qtde_v');
@@ -8,19 +10,78 @@ function valida_tipo(){
 		var qtde_v = qtde_v_field.value;
 		var graph_type= graph_type_field.value;
 
-		if(graph_type == "Grafo"){
+		if(graph_type == "1"){
 			if(qtde_a > qtde_v*qtde_v){
 				console.log('chegou');
 				swal({
 					title: "Erro",
-					text: "quantidade de vértices elevado",
+					Number: "quantidade de vértices elevado",
 					icon: "error",
 				});
-				qtde_a_field.focus();
+			
 				}
 			}
 
 		}
 
+		function clearDiv(element_id){
+			document.getElementById(element_id).innerHTML ="";
+		}
+
+function geraInputs(){
+	var qtde_v_field =document.getElementById('qtde_v');
+		var graph_type_field = document.getElementById('graph_type');
+		var qtde_a_field = document.getElementById('qtde_a');
+		var max_a;
+		var qtde_a = qtde_a_field.value;
+		var qtde_v = qtde_v_field.value;
+		var graph_type= graph_type_field.value;
+
+
+
+
+	for (var i = 0; i < qtde_a; i++) {
+		var OriginBox = document.createElement("input");
+		var DestinyBox = document.createElement("input");
+		var LabelDesBox = document.createElement("label");
+		var LabelOriBox = document.createElement("label");
+		var div = document.createElement("div");
+		var OriginTxt = document.createTextNode("Origem da aresta "+(i+1)+" :");
+		var DestinyTxt = document.createTextNode("Destino da aresta "+(i+1)+" :");
+
+		OriginBox.setAttribute("type", "number");
+		DestinyBox.setAttribute("type", "number");
+		LabelDesBox.setAttribute("class", "form-control");
+		LabelOriBox.setAttribute("class", "form-control");
+		div.setAttribute("class","col-sm-3");
+
+
+		OriginBox.setAttribute("id", "origin"+i);
+		DestinyBox.setAttribute("id", "destiny"+i);
+		LabelDesBox.setAttribute("id", "LabelDesBox"+i);
+		LabelDesBox.setAttribute("text", "Destino: ");
+		LabelOriBox.setAttribute("id", "LabelOriBox"+i);
+		div.setAttribute("id","div"+i);
+
+
+		OriginBox.setAttribute("name", "origin"+i);
+		DestinyBox.setAttribute("name", "destiny"+i);
+
+		OriginBox.setAttribute("title","origin"+i);
+		DestinyBox.setAttribute("title","destiny"+i);
+
+		OriginBox.setAttribute("class", "form-control");
+		DestinyBox.setAttribute("class", "form-control");
+
+		OriginBox.setAttribute("value", "");
+		DestinyBox.setAttribute("value", "");
+		document.getElementById("div_inputs").appendChild(div);
+		div.appendChild(LabelOriBox);
+		div.appendChild(LabelDesBox);
+		LabelOriBox.appendChild(OriginTxt);
+		LabelDesBox.appendChild(DestinyTxt);
+		LabelOriBox.appendChild(OriginBox); 
+		LabelDesBox.appendChild(DestinyBox); 
 		
-	
+}
+	}
