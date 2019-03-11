96ff2f3f59b527fc3002704813109531f3b62091
Genarodaniel <37519144+Genarodaniel@users.noreply.github.com>
2019-03-11 20:40:20 -0300

    Create functions for receive, generate, and print  a digraph(php)

--- grafo.php
+++ grafo.php
@@ -6,14 +6,53 @@ $qtde_v = isset($_POST["qtde_v"]) ? $_POST["qtde_v"] : null;
 $qtde_a = isset($_POST["qtde_a"]) ? $_POST["qtde_a"] : null;
 $graph_type = isset($_POST["graph_type"]) ? $_POST["graph_type"] : null;
 
+digrafo($qtde_v, $qtde_a, $graph_type);
+
 function geraMatrizGrafo() {
 
 }
 
-function imprime() {
+function imprime($array, $qtde_v) {
+
+	echo "<table><tbody>";
+
+	$cont = 0;
+	foreach ($array as $row) {
+		echo "<th>$cont</th>";
+
+		if ($cont == 0) {
+			for ($i = 1; $i <= $qtde_v; $i++) {
+				echo "<th>" . "$i" . "</th>";
+			}
+		}
+
+		if ($cont > 0) {
+			foreach ($row as $i => $valor) {
+				if ($i > 0 && $i <= $qtde_v) {
+					echo "<td>" . "$valor" . "</td>";
+
+				}
+			}
+		}
+
+		echo "</tr>";
+
+		$cont++;
+	}
+	echo "</table>";
 
 }
 
+function digrafo($qtde_v, $qtde_a, $graph_type) {
+	if ($graph_type == 1) {
+
+	} elseif ($graph_type == 2) {
+		$grafo = recebe($qtde_v, $qtde_a, $graph_type);
+		$grafo_gerado = geraMatrizDigrafo($grafo, $qtde_a, $qtde_v);
+		imprime($grafo_gerado, $qtde_v);
+	}
+
+}
 function recebe($qtde_v, $qtde_a, $graph_type) {
 
 	$qtde_v = trim($qtde_v);
@@ -22,7 +61,7 @@ function recebe($qtde_v, $qtde_a, $graph_type) {
 
 	if ($graph_type == 2) {
 		$digrafo = array();
-		$max_a = $qtde_v * $qtde_v / 2;
+		$max_a = $qtde_v * $qtde_v;
 
 		if ($qtde_a <= $max_a) {
 			for ($cont2 = 0; $cont2 <= $qtde_v; $cont2++) {
@@ -34,16 +73,16 @@ function recebe($qtde_v, $qtde_a, $graph_type) {
 			}
 			return $digrafo;
 		} elseif ($qtde_a > $max_a) {
-			return "Quantidade de arestas maior que o permitido para digrafo";
+			echo "<b>Quantidade de arestas maior que o permitido para digrafo Linha 77</b></br></br>";
 		} elseif ($qtde_a == 0) {
-			return "Quantidade de arestas não definida";
+			echo "<b>Quantidade de arestas não definida Linha 79</b></br>";
 		}
 	} elseif ($graph_type = 1) {
 		$grafo = array();
+		$max_a = $qtde_v * $qtde_v / 2;
 		if ($qtde_a == 0) {
-			return "Quantidade de arestas não definida";
+			echo "<b>Quantidade de arestas não definida Linha 84 </b></br></br>";
 		}
-		$max_a = $qtde_v * $qtde_v;
 
 		if ($qtde_a <= $max_a) {
 			for ($cont2 = 0; $cont2 <= $qtde_v; $cont2++) {
@@ -56,18 +95,41 @@ function recebe($qtde_v, $qtde_a, $graph_type) {
 			return $grafo;
 
 		} elseif ($qtde_a > $max_a) {
-			return "Quantidade de arestas maior que o permitido para grafo";
-		} 
+			echo "<b>Quantidade de arestas maior que o permitido para grafo Linha 98 </b></br></br>";
+		}
 
 	}
 
 }
 
-var_dump(recebe($qtde_v, $qtde_a, $graph_type));
+function geraMatrizDigrafo($array, $qtde_a, $qtde_v) {
 
-function geraMatrizDigrafo() {
+	for ($i = 0; $i < $qtde_a; $i++) {
+		$origin = isset($_POST["origin" . "$i"]) ? $_POST["origin" . "$i"] : null;
+		$destiny = isset($_POST["destiny" . "$i"]) ? $_POST["origin" . "$i"] : null;
 
-	/*foreach ($digrafo as $row) {
+		if ($origin > 0 & $origin <= $qtde_v) {
+			$array[$origin][$destiny] = 1;
+		} else {
+			echo "<b>dados inconsistentes(origem)Linha 114 Loop num $i</b></br></br>";
+
+		}
+		if ($destiny > 0 & $destiny <= $qtde_v) {
+			$array[$origin][$destiny] = 1;
+		} else {
+			echo "<b>dados inconsistentes(Destino) Linha 120 Loop num $i</b></br></br>";
+		}
+
+	}
+	return $array;
+
+}
+
+// variaveis origem e destino devem ser diferentes de zero e menor
+// ou igual $qtde_v
+?>
+
+ <!--/*foreach ($digrafo as $row) {
 		// entra no vetor digrafo trazendo as posições para row
 
 		foreach ($row as $key => $value) {
@@ -84,13 +146,7 @@ function geraMatrizDigrafo() {
 			}
 		}
 	}*/
-
-}
-
-// variaveis origem e destino devem ser diferentes de zero e menor
-// ou igual $qtde_v
-?>
-
+-->
 
 
 	<!-- <table><tbody><?php/*
